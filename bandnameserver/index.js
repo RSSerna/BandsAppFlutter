89254854fa094a97e35fbacc56101d64a4610408
RicardoSaldarriagaS <=>
2024-02-07 13:12:23 -0500

    Created and Linked HTML file to Node.
    Now having a UI

--- bandnameserver/index.js
+++ bandnameserver/index.js
@@ -1,6 +1,12 @@
-const e = require('express');
+const path = require('path');
 const express = require('express');
+
 const app = express();
+
+//Path Publico
+const publicPath = path.resolve(__dirname, 'public');
+
+app.use(express.static(publicPath));
 
 app.listen(3000, (err) => {
     if (err) throw new Error(err);
