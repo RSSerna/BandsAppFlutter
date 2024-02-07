833ce17f2da7b3f8f04b489184256262c1f6a473
RicardoSaldarriagaS <=>
2024-02-07 13:35:05 -0500

    Configuracion Inicial del NodeJs con el html usando Sockets

--- bandnameserver/index.js
+++ bandnameserver/index.js
@@ -2,14 +2,26 @@ const path = require('path');
 const express = require('express');
 require('dotenv').config();
 
+//App de Express
 const app = express();
+
+//Node Server
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+//Mensajes por Sockets
+io.on('connection', client => {
+    console.log('Cliente Conectado');
+    client.on('disconnect', () => { console.log('Cliente desconectado'); });
+});
+
 
 //Path Publico
 const publicPath = path.resolve(__dirname, 'public');
-
 app.use(express.static(publicPath));
 
-app.listen(process.env.PORT, (err) => {
+
+server.listen(process.env.PORT, (err) => {
     if (err) throw new Error(err);
     console.log('Servidor corriendo en puerto', process.env.PORT);
 });
