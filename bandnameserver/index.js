601443e851ac08e827a7c6a19ec74a88608d4b03
RicardoSaldarriagaS <=>
2024-02-07 13:05:57 -0500

    Created Node Js Server

---
+++ bandnameserver/index.js
@@ -0,0 +1,8 @@
+const e = require('express');
+const express = require('express');
+const app = express();
+
+app.listen(3000, (err) => {
+    if (err) throw new Error(err);
+    console.log('Servidor corriendo en puerto', 3000);
+});
