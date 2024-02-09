339a47fc31ddbe4d3d7e0949fd80f8dbcb986b60
RicardoSaldarriagaS <=>
2024-02-09 11:16:26 -0500

    Created Models and "Saving info"

--- bandnameserver/sockets/socket.js
+++ bandnameserver/sockets/socket.js
@@ -1,8 +1,20 @@
 const { io } = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('AC/DC'));
+bands.addBand(new Band('Metallica'));
+bands.addBand(new Band('Linkedin Park'));
+
+console.log(bands);
 
 //Mensajes por Sockets
 io.on('connection', client => {
     console.log('Cliente Conectado');
+    client.emit('active-bands', bands.getBands());
     client.on('disconnect', () => { console.log('Cliente desconectado'); });
     client.on('mensaje', (payload) => {
         console.log('Mensaje: ', payload);
